369e8adb52d82df46d94a1ab563f6b643b91a0f3
EllaLee233 <lmlll2293068536@gmail.com>
2024-09-16 09:55:42 -0400

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by apple on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
